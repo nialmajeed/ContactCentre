959408d3c810917ff4c708e767289b03d27a22c5
Nial Majeed <nial.maj@gmail.com>
2025-05-04 09:00:51 +0100

    update function to current working function in aws for basic data dip

--- CE_Foundational/DataDipLambda.py
+++ CE_Foundational/DataDipLambda.py
@@ -1,32 +1,21 @@
 import boto3
 import json
 
-dynamodb = boto3.client('dynamodb')
+dynamodb = boto3.resource('dynamodb')
 
 def lambda_handler(event, context):
     # Extract phone number from Contact Flow event
     phone_number = event['Details']['ContactData']['CustomerEndpoint']['Address']
+    print(phone_number)
 
     # Query DynamoDB table
     try:
-        response = dynamodb.query(
-            TableName='C-Foundation-Table',  # Replace with your table name
-            KeyConditionExpression='"Phone Number" = :p',
-            ExpressionAttributeValues={
-                ':p': {'S': "Phone Number"}
-            }
-        )
+        TableName= dynamodb.Table('C-Foundation-Table')  # Replace with your table name
+        dbResponse = TableName.get_item( Key = {'PhoneNumber': str(phone_number)})
+        dbFname = dbResponse['Item']['FirstName']
+        return {'FirstName': dbFname}
 
- if response['Items']:
-            item = response['Items'][0]  # Assuming only one match per phone number
-            customer_name = f"{item.get('FirstName', {}).get('S', '')} {item.get('LastName', {}).get('S', '')}"
-            return {'customerName': customer_name}
-        else:
-            return {'customerName': ' '}
 
     except Exception as e:
         print(f"Error: {e}")
-        return {'customerName': 'Error'}
-
-
-    
+        return {'FirstName': 'Error'}
